b810a595a4dfa4d3b81c320b470ea0b35a20ac3b
ravi-braj <fastbucks79@gmail.com>
2018-01-07 18:25:04 +1100

    yes

--- market/models.py
+++ market/models.py
@@ -40,6 +40,7 @@ def save_user_profile(sender, instance, **kwargs):
 model for representing stocks. Used to store the offline list of symbols to call to the alpha Q API and their corresponding names
 """
 class Stock(models.Model):
+	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length = 250, help_text="Enter the name of stock")
 	symbol = models.CharField(max_length = 20, help_text = "Enter the stocks name")
 
